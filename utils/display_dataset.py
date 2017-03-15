fb3224aed4d040b8650de856af05ce9d791904ed
alex <sashaudalv@gmail.com>
2017-03-15 19:38:23 +0300

    dataset showing 500 digits

--- utils/display_dataset.py
+++ utils/display_dataset.py
@@ -6,7 +6,7 @@ digits = datasets.load_digits()
 
 # Display the first digit
 plt.figure(1, figsize=(3, 3))
-for i in range(0, 100):
-    plt.subplot(10, 10, i + 1).axis('off')
+for i in range(0, 500):
+    plt.subplot(10, 50, i + 1).axis('off')
     plt.imshow(digits.images[i], cmap=plt.cm.gray_r, interpolation='nearest')
 plt.show()
