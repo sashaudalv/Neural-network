37c2d2ef8ac3eb43f484dd87045c53269b1d68e8
alex <sashaudalv@gmail.com>
2017-03-17 01:01:03 +0300

    number training samples changing at console argument

--- run.py
+++ run.py
@@ -7,17 +7,19 @@ import matplotlib.pyplot as plt
 from sklearn import datasets, metrics
 from sklearn.neural_network import MLPClassifier
 
+# The digits dataset
+digits = datasets.load_digits()
+n_samples = len(digits.images)
 
-def run(learning_rate):
-    # The digits dataset
-    digits = datasets.load_digits()
 
-    # The data that we are interested in is made of 8x8 images of digits, let's
-    # have a look at the first 4 images, stored in the `images` attribute of the
-    # dataset.  If we were working from image files, we could load them using
-    # matplotlib.pyplot.imread.  Note that each image must have the same size. For these
-    # images, we know which digit they represent: it is given in the 'target' of
-    # the dataset.
+def run(learning_rate, n_training_samples=n_samples // 2):
+    if learning_rate <= 0 or learning_rate > 1 or n_training_samples < 1 \
+            or n_training_samples > n_samples - 20:
+        print('learning-rate must take values (0;1]\n'
+              'n-training-samples must take int values [1;%i]' % (n_samples - 20))
+        return
+
+    # The data is made of 8x8 images of digits
     images_and_labels = list(zip(digits.images, digits.target))
     for index, (image, label) in enumerate(images_and_labels[:20]):
         plt.subplot(4, 10, index + 1)
@@ -27,25 +29,24 @@ def run(learning_rate):
 
     # To apply a classifier on this data, we need to flatten the image, to
     # turn the data in a (samples, feature) matrix:
-    n_samples = len(digits.images)
     data = digits.images.reshape((n_samples, -1))
 
     # Create a classifier: a support vector classifier
-    classifier = MLPClassifier(solver='sgd', activation='logistic', hidden_layer_sizes=(32, ),
+    classifier = MLPClassifier(solver='sgd', activation='logistic', hidden_layer_sizes=(32,),
                                learning_rate='constant', learning_rate_init=learning_rate, verbose=True)
 
     # We learn the digits on the first half of the digits
-    classifier.fit(data[:n_samples // 2], digits.target[:n_samples // 2])
+    classifier.fit(data[:n_training_samples], digits.target[:n_training_samples])
 
     # Now predict the value of the digit on the second half:
-    expected = digits.target[n_samples // 2:]
-    predicted = classifier.predict(data[n_samples // 2:])
+    expected = digits.target[n_training_samples:]
+    predicted = classifier.predict(data[n_training_samples:])
 
     print("Classification report for classifier %s:\n%s\n"
           % (classifier, metrics.classification_report(expected, predicted)))
     print("Confusion matrix:\n%s" % metrics.confusion_matrix(expected, predicted))
 
-    images_and_predictions = list(zip(digits.images[n_samples // 2:], predicted))
+    images_and_predictions = list(zip(digits.images[n_training_samples:], predicted))
     for index, (image, prediction) in enumerate(images_and_predictions[:20]):
         plt.subplot(4, 10, index + 21)
         plt.axis('off')
