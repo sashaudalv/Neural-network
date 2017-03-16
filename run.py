a25fcfe9bb586559bd2d3e8e00dcef6bc176625a
alex <sashaudalv@gmail.com>
2017-03-17 00:07:13 +0300

    import python-fire

--- run.py
+++ run.py
@@ -1,3 +1,5 @@
+import fire
+
 # Standard scientific Python imports
 import matplotlib.pyplot as plt
 
@@ -5,47 +7,57 @@ import matplotlib.pyplot as plt
 from sklearn import datasets, metrics
 from sklearn.neural_network import MLPClassifier
 
-# The digits dataset
-digits = datasets.load_digits()
 
-# The data that we are interested in is made of 8x8 images of digits, let's
-# have a look at the first 4 images, stored in the `images` attribute of the
-# dataset.  If we were working from image files, we could load them using
-# matplotlib.pyplot.imread.  Note that each image must have the same size. For these
-# images, we know which digit they represent: it is given in the 'target' of
-# the dataset.
-images_and_labels = list(zip(digits.images, digits.target))
-for index, (image, label) in enumerate(images_and_labels[:20]):
-    plt.subplot(4, 10, index + 1)
-    plt.axis('off')
-    plt.imshow(image, cmap=plt.cm.gray_r, interpolation='nearest')
-    plt.title('Training: %i' % label)
+def run(learning_rate):
+    # The digits dataset
+    digits = datasets.load_digits()
 
-# To apply a classifier on this data, we need to flatten the image, to
-# turn the data in a (samples, feature) matrix:
-n_samples = len(digits.images)
-data = digits.images.reshape((n_samples, -1))
+    # The data that we are interested in is made of 8x8 images of digits, let's
+    # have a look at the first 4 images, stored in the `images` attribute of the
+    # dataset.  If we were working from image files, we could load them using
+    # matplotlib.pyplot.imread.  Note that each image must have the same size. For these
+    # images, we know which digit they represent: it is given in the 'target' of
+    # the dataset.
+    images_and_labels = list(zip(digits.images, digits.target))
+    for index, (image, label) in enumerate(images_and_labels[:20]):
+        plt.subplot(4, 10, index + 1)
+        plt.axis('off')
+        plt.imshow(image, cmap=plt.cm.gray_r, interpolation='nearest')
+        plt.title('Training: %i' % label)
 
-# Create a classifier: a support vector classifier
-classifier = MLPClassifier(solver='sgd', activation='logistic', hidden_layer_sizes=(32, ),
-                           learning_rate='constant', learning_rate_init=0.5, verbose=True)
+    # To apply a classifier on this data, we need to flatten the image, to
+    # turn the data in a (samples, feature) matrix:
+    n_samples = len(digits.images)
+    data = digits.images.reshape((n_samples, -1))
 
-# We learn the digits on the first half of the digits
-classifier.fit(data[:n_samples // 2], digits.target[:n_samples // 2])
+    # Create a classifier: a support vector classifier
+    classifier = MLPClassifier(solver='sgd', activation='logistic', hidden_layer_sizes=(32, ),
+                               learning_rate='constant', learning_rate_init=learning_rate, verbose=True)
 
-# Now predict the value of the digit on the second half:
-expected = digits.target[n_samples // 2:]
-predicted = classifier.predict(data[n_samples // 2:])
+    # We learn the digits on the first half of the digits
+    classifier.fit(data[:n_samples // 2], digits.target[:n_samples // 2])
 
-print("Classification report for classifier %s:\n%s\n"
-      % (classifier, metrics.classification_report(expected, predicted)))
-print("Confusion matrix:\n%s" % metrics.confusion_matrix(expected, predicted))
+    # Now predict the value of the digit on the second half:
+    expected = digits.target[n_samples // 2:]
+    predicted = classifier.predict(data[n_samples // 2:])
 
-images_and_predictions = list(zip(digits.images[n_samples // 2:], predicted))
-for index, (image, prediction) in enumerate(images_and_predictions[:20]):
-    plt.subplot(4, 10, index + 21)
-    plt.axis('off')
-    plt.imshow(image, cmap=plt.cm.gray_r, interpolation='nearest')
-    plt.title('Prediction: %i\nTrue: %i' % (prediction, expected[index]))
+    print("Classification report for classifier %s:\n%s\n"
+          % (classifier, metrics.classification_report(expected, predicted)))
+    print("Confusion matrix:\n%s" % metrics.confusion_matrix(expected, predicted))
 
-plt.show()
+    images_and_predictions = list(zip(digits.images[n_samples // 2:], predicted))
+    for index, (image, prediction) in enumerate(images_and_predictions[:20]):
+        plt.subplot(4, 10, index + 21)
+        plt.axis('off')
+        plt.imshow(image, cmap=plt.cm.gray_r, interpolation='nearest')
+        plt.title('Prediction: %i\nTrue: %i' % (prediction, expected[index]))
+
+    plt.show()
+
+
+def main():
+    fire.Fire(run)
+
+
+if __name__ == '__main__':
+    main()
